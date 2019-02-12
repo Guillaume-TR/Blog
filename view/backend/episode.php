95997cf8876f16bc98ff3a279bd9e17c28e12283
Guillaume Terrien <guillaume.terrien.gt@gmail.com>
2019-02-12 14:43:28 +0100

    Update the site theme: home and episode page on the admin panel.

--- view/backend/episode.php
+++ view/backend/episode.php
@@ -1,41 +1,29 @@
 <?php $this->title = 'Gérez les épisodes'; ?>
-
 <header id="head" class="secondary"></header>
 
 <div class="container">
-
-    <ol class="breadcrumb">
-        <li><a href="index.php?page=admin">Panel d'administration</a></li>
-        <li class="active"><?= $this->title ?></li>
-    </ol>
-
-    <div class="row">
-        <article class="col-md-12 maincontent list-admin">
-
-            <header class="page-header">
-                <h1 class="page-title"><?= $this->title ?></h1>
-            </header>
-
-            <a href="index.php?page=admin&action=addEpisode" class="btn btn-success">Ajouter un épisode</a>
-
-			<?php foreach ($episodes as $episode): ?>
-                <div class="jumbotron top-space episode-manage">
-
-                    <h2><?= $episode->getTitle(); ?></h2>
-
-                    <div class="episode-content"><?= substr($episode->getContent(), 0, 200) . '...'; ?></div>
-
-                    <p class="text-right">
-                        <a href="index.php?page=admin&action=editEpisode&id=<?= $episode->getId(); ?>"
-                           class="btn btn-warning">Modifier</a>
-                        <a href="index.php?page=admin&action=deleteEpisode&id=<?= $episode->getId(); ?>"
-                           class="btn btn-danger">Supprimer</a>
-                    </p>
-
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="index.php">Accueil</a></li>
+            <li class="breadcrumb-item active" aria-current="page"><?= $this->title ?></li>
+        </ol>
+    </nav>
+    <article class="col-md-12 maincontent">
+        <header class="page-header">
+            <h1 class="page-title"><?= $this->title ?></h1>
+        </header>
+        <a href="index.php?page=admin&action=addEpisode" class="btn btn-success btn-lg">Ajouter un épisode</a>
+		<?php foreach ($episodes as $episode): ?>
+            <div class="card my-3">
+                <h4 class="card-header"><?= $episode->getTitle(); ?></h4>
+                <div class="card-body">
+                    <p class="card-text"><?= substr($episode->getContent(), 0, 300) . '...'; ?></p>
+                    <a href="index.php?page=admin&action=editEpisode&id=<?= $episode->getId(); ?>"
+                       class="btn btn-info btn-lg m-2">Modifier</a>
+                    <a href="index.php?page=admin&action=deleteEpisode&id=<?= $episode->getId(); ?>"
+                       class="btn btn-danger btn-lg m-2">Supprimer</a>
                 </div>
-			<?php endforeach; ?>
-
-        </article>
-    </div>
-
+            </div>
+		<?php endforeach; ?>
+    </article>
 </div>
--- view/backend/home.php
+++ view/backend/home.php
@@ -1,24 +1,31 @@
 <?php $this->title = 'Panel d\'administration'; ?>
-
 <header id="head" class="secondary"></header>
 
 <div class="container">
-    <ol class="breadcrumb">
-        <li class="active"><?= $this->title ?></li>
-    </ol>
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item active" aria-current="page"><?= $this->title ?></li>
+        </ol>
+    </nav>
     <div class="row">
         <article class="col-md-12 maincontent">
             <header class="page-header">
-                <h1 class="page-title"><?= $this->title ?></h1>
+                <h1 class="page-title text-center"><?= $this->title ?></h1>
             </header>
-            <h2><a href="index.php?page=admin&action=episode">Gérez les épisodes</a></h2>
-            <blockquote>Gestion des épisodes du livre "Billet simple pour l'Alaska". Ajouter un épisode, modifier un épisode ou supprimer un épisode.
+            <blockquote class="blockquote">
+                <p class="mb-0"><h2><a href="index.php?page=admin&action=episode">Gérez les épisodes</a></h2></p>
+                <footer class="blockquote-footer">Gestion des épisodes du livre "Billet simple pour l'Alaska".
+                    Ajouter un épisode, modifier un épisode ou supprimer un épisode.</footer>
             </blockquote>
-            <h2><a href="index.php?page=admin&action=account">Gérez les utilisateurs</a></h2>
-            <blockquote>Gestion des utilisateurs du site. Ajouter un utilisateur, modifier un utilisateur ou supprimer un utilisateur.
+            <blockquote class="blockquote">
+                <p class="mb-0"><h2><a href="index.php?page=admin&action=account">Gérez les utilisateurs</a></h2></p>
+                <footer class="blockquote-footer">Gestion des utilisateurs du site.
+                    Ajouter un utilisateur, modifier un utilisateur ou supprimer un utilisateur.</footer>
             </blockquote>
-            <h2><a href="index.php?page=admin&action=comment">Gérez les commentaires</a></h2>
-            <blockquote>Gestion des commentaires des épisodes. Vérifier les commentaires signalé, approuver un commentaires signalé ou supprimer un commentaire.
+            <blockquote class="blockquote">
+                <p class="mb-0"><h2><a href="index.php?page=admin&action=comment">Gérez les commentaires</a></h2></p>
+                <footer class="blockquote-footer">Gestion des commentaires postés sur les épisodes.
+                    Vérifier les commentaires signalé, approuver un commentaires signalé ou supprimer un commentaire.</footer>
             </blockquote>
         </article>
     </div>
